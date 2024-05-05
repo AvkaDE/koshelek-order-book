8d1ea59696e8204c3e6c44aabb24c8ddc28d892d
Алексей <lol2012pw@yandex.ru>
2024-05-05 23:27:29 +0300

    feat: add mock table for order page

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,9 +1,30 @@
 <template>
-  <main>
-    <div class="order-page">
-      <h1>This is an order page</h1>
-    </div>
-  </main>
+  <v-container fluid>
+    <v-row>
+      <v-col md="6"> BIDS: </v-col>
+      <v-col md="6"> ASKS: </v-col>
+    </v-row>
+    <v-row>
+      <v-col md="6">
+        <v-data-table
+          :fixed-header="true"
+          :items="mockAsks"
+          :items-per-page="100"
+          :items-per-page-options="[100, 500, 1000]"
+          height="75vh"
+        ></v-data-table>
+      </v-col>
+      <v-col md="6">
+        <v-data-table
+          :fixed-header="true"
+          :items="mockAsks"
+          :items-per-page="100"
+          :items-per-page-options="[100, 500, 1000]"
+          height="75vh"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -12,7 +33,169 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'OrderView',
   setup() {
-    return {}
+    const mockAsks = [
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      },
+      {
+        price: 123,
+        quantity: 3,
+        total: 1213
+      }
+    ]
+    return { mockAsks }
   }
 })
 </script>
